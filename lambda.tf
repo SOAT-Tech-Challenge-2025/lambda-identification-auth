68e1547425f35890332350453c4c12f15ad6f9de
carlosbastoslaet <carlosbastoslaet@gmail.com>
2025-10-06 19:50:52 -0300

    fix lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -1,28 +1,11 @@
-resource "aws_iam_role" "lambda_exec_role" {
+data "aws_iam_role" "lambda_exec_role" {
   name = "tc-infra-id-lambda-exec-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
 
 resource "aws_lambda_function" "id_lambda" {
   function_name = "lambda-identification-auth"
-  depends_on    = [aws_iam_role_policy_attachment.lambda_logs]
-  role          = aws_iam_role.lambda_exec_role.arn
+  role          = data.aws_iam_role.lambda_exec_role.arn
   handler       = "tech.buildrun.lambda.Handler::handleRequest"
   runtime       = "java17"
 
