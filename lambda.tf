d97b852c4db0419658d24a16cbc9a67a4ba68ff8
carlosbastoslaet <carlosbastoslaet@gmail.com>
2025-12-16 22:24:39 -0300

    fix lambda

--- lambda.tf
+++ lambda.tf
@@ -3,19 +3,8 @@ data "aws_iam_role" "lambda_exec_role" {
 }
 
 
-resource "aws_iam_role_policy_attachment" "lambda_logs" {
-  role       = data.aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_vpc_access" {
-  role       = data.aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
-}
-
 resource "aws_lambda_function" "id_lambda" {
   function_name = "lambda-identification-auth"
-  depends_on    = [aws_iam_role_policy_attachment.lambda_logs]
   role          = data.aws_iam_role.lambda_exec_role.arn
   handler       = "tech.buildrun.lambda.Handler::handleRequest"
   runtime       = "java17"
@@ -33,20 +22,4 @@ resource "aws_lambda_function" "id_lambda" {
   }
 
   tags = var.tags
-}
-
-resource "aws_lambda_permission" "apigw_invoke_lambda" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.id_lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${data.aws_apigatewayv2_api.tc_api.execution_arn}/*/*"
-}
-
-resource "aws_security_group_rule" "id_lambda_to_rds" {
-  type                     = "ingress"
-  from_port                = 5432
-  to_port                  = 5432
-  protocol                 = "tcp"
-  security_group_id        = data.aws_security_group.rds.id
 }
