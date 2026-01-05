7b88567dc8e9537b66a8fc682624306daa15ec40
carlosbastoslaet <carlosbastoslaet@gmail.com>
2026-01-05 20:28:56 -0300

    fix lambda

--- lambda.tf
+++ lambda.tf
@@ -2,8 +2,19 @@ data "aws_iam_role" "lambda_exec_role" {
   name = "tc-infra-id-lambda-exec-role"
 }
 
-data "aws_security_group" "id_lambda" {
-  name = "tc-id-lambda-sg"
+resource "aws_security_group" "id_lambda" {
+  name        = "tc-id-lambda-sg"
+  description = "Security group for Lambda ID function"
+  vpc_id      = data.aws_vpc.tc_lambda_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = var.tags
 }
 
 resource "aws_lambda_function" "id_lambda" {
